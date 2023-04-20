88785b15efd814d6f228e44d0b5f435fec8585cf
Stephane Njoki <69685769+Kinjuriu@users.noreply.github.com>
2023-04-20 22:38:17 +0300

    Update model.py

--- model.py
+++ model.py
@@ -208,7 +208,7 @@ def _inverted_res_block(inputs, expansion, stride, alpha, filters, block_id, ski
     return x
 
 
-def Deeplabv3(weights='pascal_voc', input_tensor=None, input_shape=(512, 512, 24), classes=21, backbone='mobilenetv2',
+def Deeplabv3(weights='pascal_voc', input_tensor=None, input_shape=(265, 256, 24), classes=21, backbone='mobilenetv2',
               OS=16, alpha=1., activation=None):
     """ Instantiates the Deeplabv3+ architecture
     Optionally loads weights pre-trained
